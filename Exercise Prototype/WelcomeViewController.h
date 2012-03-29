45bc7e7cd0b9a02e74a30403954a6da9c3af335f
Fuutou Tan <shengdongtan@gmail.com>
2012-03-29 23:37:55 +0800

    Added Snarkdown

--- Exercise Prototype/WelcomeViewController.h
+++ Exercise Prototype/WelcomeViewController.h
@@ -7,7 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
 
 @interface WelcomeViewController : UIViewController
+
+@property (nonatomic, strong) UIManagedDocument *eDatabase;
 
 @end
--- Exercise Prototype/WelcomeViewController.m
+++ Exercise Prototype/WelcomeViewController.m
@@ -10,10 +10,64 @@
 
 @implementation WelcomeViewController
 
+@synthesize eDatabase = _eDatabase;
 
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+#pragma mark - Database Usage
+
+
+
+// 3. Open or create the document here
+// We modify one which is much more generate                                                                                                       
+
+- (void)useDocument
 {
-    return NO;
+    if (![[NSFileManager defaultManager] fileExistsAtPath:[self.eDatabase.fileURL path]]) {
+        //does not exist on disk, so create it 
+        [self.eDatabase saveToURL:self.eDatabase.fileURL forSaveOperation:UIDocumentSaveForCreating completionHandler:^(BOOL success){
+            NSLog(@"The database is created");
+        }];
+    } else if (self.eDatabase.documentState == UIDocumentStateClosed) {
+        // exists on disk, but we need to open it 
+        [self.eDatabase openWithCompletionHandler:^(BOOL success){
+            NSLog(@"The database is opened");
+        }];
+    } else if (self.eDatabase.documentState == UIDocumentStateNormal) {
+        // already open and ready to use
+        NSLog(@"The database is already opened");
+    }
 }
+
+#pragma mark - Setter
+
+// 2.Init our database
+
+- (void)setEDatabase:(UIManagedDocument *)eDatabase
+{
+    if (_eDatabase != eDatabase) {
+        _eDatabase = eDatabase;
+        [self useDocument];
+    }
+}
+
+
+
+#pragma mark - View Controller LifeCycle
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    // 1.Everything start from here
+    
+    if (!self.eDatabase) {
+        // Create a default database if none is set
+        NSURL *url = [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
+        url = [url URLByAppendingPathComponent:@"Default E Database"];
+        // url is now "<Default E Database>/Default E Database"
+        self.eDatabase = [[UIManagedDocument alloc] initWithFileURL:url];
+        // setter will create this for us on disk
+    }
+}
+
 
 @end
