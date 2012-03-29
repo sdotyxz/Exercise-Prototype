139a507c0c04f706681c5674fca539f3605f43da
Fuutou Tan <shengdongtan@gmail.com>
2012-03-29 19:01:14 +0800

    first commit

---
+++ Exercise Prototype/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Exercise Prototype
+//
+//  Created by Tan Fuutou on 12-03-29.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Exercise Prototype/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Exercise Prototype
+//
+//  Created by Tan Fuutou on 12-03-29.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
